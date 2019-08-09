a7e65ac3f5ba9312d2246bbf0a84651139880903
Panji28 <panjiprawirowibowo@gmail.com>
2019-08-09 16:19:11 +0700

    Tugas 7 javascript
    Tugas 7 js

---
+++ tugas7.js
@@ -0,0 +1,13 @@
+
+var tinggiBadan = [110,120,130,140,150,151,152,153,154,155]
+while( tinggiBadan <= 10){
+    consolole.log(tinggiBadan)
+    tinggiBadan++;
+}
+
+
+
+var tinggiBadan = [110,120,130,140,150,151,152,153,154,155]
+for(let x of tinggiBadan){
+    console.log("Tinggi badan",x)
+}
